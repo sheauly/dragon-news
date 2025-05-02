be06eb23f4bbb6de92004841bbb8d3c03af9eae2
halimawebd <inthesumon1010@gmail.com>
2025-05-03 00:35:53 +0600

    import the header part

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
-// import React from 'react';
-// import logo from '../../src/assets/logo.png'
-// import { format } from 'date-fns';
-// const Header = () => {
-//     return (
-//         <div  className='flex justify-center items-center flex-col gap-3'>
-//             <img className='w-[350px]' src={logo} alt="" />
-//             <p className='text-accent'>Journalism Without Fear or Favour</p>
+import React from 'react';
+import logo from '../../src/assets/logo.png'
+import { format } from 'date-fns';
+const Header = () => {
+    return (
+        <div  className='flex justify-center items-center flex-col gap-3'>
+            <img className='w-[350px]' src={logo} alt="" />
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
             
-//             <p className='text-semibold text-accent'>{format(new Date(), "EEEE, MMMM MM, yyyy")}</p>
-//         </div>
-//     );
-// };
+            <p className='text-semibold text-accent'>{format(new Date(), "EEEE, MMMM MM, yyyy")}</p>
+        </div>
+    );
+};
 
-// export default Header;
+export default Header;
