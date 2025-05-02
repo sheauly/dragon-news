bf2f87eae124899f3379b74d12d49a12a5c63785
halimawebd <inthesumon1010@gmail.com>
2025-05-03 00:43:42 +0600

    add the card and button

--- src/layouts/LeftAside.jsx
+++ src/layouts/LeftAside.jsx
@@ -1,15 +1,15 @@
-// import React, { Suspense } from 'react';
-// import Categories from './Categories';
+import React, { Suspense } from 'react';
+import Categories from './Categories';
 
-// const LeftAside = () => {
-//     return (
-//         <div>
-//             <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}>
-//                 <Categories></Categories>
-//             </Suspense>
+const LeftAside = () => {
+    return (
+        <div>
+            <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}>
+                <Categories></Categories>
+            </Suspense>
             
-//         </div>
-//     );
-// };
-// // 
-// export default LeftAside;
+        </div>
+    );
+};
+// 
+export default LeftAside;
--- src/layouts/SocialLogin.jsx
+++ src/layouts/SocialLogin.jsx
@@ -1,17 +1,17 @@
-// import React from 'react';
-// import { FaGithub } from 'react-icons/fa';
-// import { FcGoogle } from "react-icons/fc";
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from "react-icons/fc";
 
-// const SocialLogin = () => {
-//     return (
-//         <div>
-//             <h2 className='font-bold'>Login With</h2>
-//             <div className='space-y-3'>
-//                 <button className='btn btn-outline btn-secondary w-full'><FcGoogle size={25}/>Login With Google</button>
-//             <button className='btn btn-outline btn-primary w-full'><FaGithub size={25} />Login With Github</button>
-//             </div>
-//         </div>
-//     );
-// };
+const SocialLogin = () => {
+    return (
+        <div>
+            <h2 className='font-bold'>Login With</h2>
+            <div className='space-y-3'>
+                <button className='btn btn-outline btn-secondary w-full'><FcGoogle size={25}/>Login With Google</button>
+            <button className='btn btn-outline btn-primary w-full'><FaGithub size={25} />Login With Github</button>
+            </div>
+        </div>
+    );
+};
 
-// export default SocialLogin;
+export default SocialLogin;
