89209d5a0c3e84e7f81e03b4c338d02e6189401f
halimawebd <inthesumon1010@gmail.com>
2025-05-03 00:39:27 +0600

    complete the categoryNews

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,37 +1,37 @@
-// import React, { useEffect, useState } from 'react';
-// import { useLoaderData, useParams } from 'react-router';
-// import NewsCard from '../components/NewsCard';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
+import NewsCard from '../components/NewsCard';
 
-// const CategoryNews = () => {
-//     const { id } = useParams();
-//     const data = useLoaderData();
+const CategoryNews = () => {
+    const { id } = useParams();
+    const data = useLoaderData();
 
-//     const [categoryNews, setCategoryNews] = useState([]);
+    const [categoryNews, setCategoryNews] = useState([]);
     
-//     useEffect(() => {
-//         if (id == "0") {
-//             setCategoryNews(data);
-//         }
-//         else if (id == "1") {
-//             const filteredNews = data.filter((news) => news.others.is_today_pick == true)
+    useEffect(() => {
+        if (id == "0") {
+            setCategoryNews(data);
+        }
+        else if (id == "1") {
+            const filteredNews = data.filter((news) => news.others.is_today_pick == true)
 
-//             setCategoryNews(filteredNews)
-//         } else {
-//             const filteredNews = data.filter((news) => news.category_id == id);
-//             setCategoryNews(filteredNews)
-//         }
-//         console.log(id, data)
+            setCategoryNews(filteredNews)
+        } else {
+            const filteredNews = data.filter((news) => news.category_id == id);
+            setCategoryNews(filteredNews)
+        }
+        console.log(id, data)
 
-//     }, [id, data])
-//     return <div className='font-bold mb-5'>
-//         <h2>Total <span className='text-secondary'>{categoryNews.length}</span> news Found</h2>
-//         <div className='grid grid-cols-1'>
-//             {
-//                 categoryNews.map(news => <NewsCard key={news.id} news={news}></NewsCard>)
-//             }
-//         </div>
-//     </div>
+    }, [id, data])
+    return <div className='font-bold mb-5'>
+        <h2>Total <span className='text-secondary'>{categoryNews.length}</span> news Found</h2>
+        <div className='grid grid-cols-1'>
+            {
+                categoryNews.map(news => <NewsCard key={news.id} news={news}></NewsCard>)
+            }
+        </div>
+    </div>
    
-// };
+};
 
-// export default CategoryNews;
+export default CategoryNews;
